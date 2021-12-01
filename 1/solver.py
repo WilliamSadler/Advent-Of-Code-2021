32eb8eb25bab97297bc6902d74fc04d50661c779
William Sadler <WGSadler@gmail.com>
2021-12-01 14:02:12 +0000

    Day 1

---
+++ 1/solver.py
@@ -0,0 +1,16 @@
+#Link to challenge: https://adventofcode.com/2021/day/1
+
+lines = []
+with open('input.txt') as f:
+    lines = f.readlines()
+
+count = 0
+previous_number = int(lines[0].strip())
+
+for i in range(1, len(lines)):
+    if int(lines[i].strip()) > previous_number:
+        count = count + 1
+    previous_number = int(lines[i].strip())
+
+print("Total number of increases: " + str(count))
+    
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Advent-Of-Code-2021
 Advent of Code 2021 Solutions SHEEEEEEEEEEEEEEEESH
+
+https://adventofcode.com/2021
